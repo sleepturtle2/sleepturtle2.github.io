005930e1896f8e92a4cc314ad46c6996671eaaa6
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-07-17 19:29:49 +0530

    added themed image functionality

--- default.js
+++ default.js
@@ -1,5 +1,7 @@
 const chk = document.getElementById('chk');
-
+const github = document.getElementById('github-image');
+const leetcode = document.getElementById('leetcode-image');
+const linkedin = document.getElementById('linkedin-image');
 
 
 chk.addEventListener('change', () => {
@@ -7,7 +9,14 @@ chk.addEventListener('change', () => {
 
     if (chk.checked) { //dark theme
         document.getElementById('theme-style').href = "dark-theme.css";
+        github.src = "images/github-logo-dark.png";
+        leetcode.src = "images/leetcode-logo-dark.png";
+        linkedin.src = "images/linkedin-logo-dark.png";
     } else { //change back to light theme
         document.getElementById('theme-style').href = "";
+        github.src = "images/github-logo-light.png";
+        leetcode.src = "images/leetcode-logo-light.png";
+        linkedin.src = "images/linkedin-logo-light.jpg";
     }
+
 });
